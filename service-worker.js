e736693be3be26b0050d5da2994431d8ddac6629
JaybaGames <jperandio@feudiliman.edu.ph>
2025-12-23 14:45:30 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+// service-worker.js
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        './',
+		'./index.html',
+		'./script.js',
+		'./style.css',
+		// Dino model
+		'./models/dino/scene.gltf',
+		'./models/dino/scene.bin',
+		'./models/dino/textures/material_baseColor.jpeg',
+		'./models/dino/textures/Skull_baseColor.jpeg',
+		// Eye model
+		'./models/eye/scene.gltf',
+		'./models/eye/scene.bin',
+		'./models/eye/textures/CORNEA_normal.png',
+		'./models/eye/textures/IRIS_FINAL_baseColor.jpeg',
+		'./models/eye/textures/IRIS_FINAL_metallicRoughness.png',
+		'./models/eye/textures/IRIS_FINAL_normal.jpeg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
